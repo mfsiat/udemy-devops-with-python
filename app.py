a5f4d99de84e384a0b858465daf42939b6491c93
mfsiat <nasirul.islam@northsouth.edu>
2021-10-23 16:55:28 +0600

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return "Hi Everyone"
+
+# base
+@app.route('/base')
+def home1():
+    return render_template("base.html")
+
+if __name__ == "__main__":
+    app.run()
