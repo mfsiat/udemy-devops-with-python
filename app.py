50dc2136ec355580396cbb0c45cfc0cce4e998b8
mfsiat <nasirul.islam@northsouth.edu>
2021-10-23 17:52:33 +0600

    add database & gitignore

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db=SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 @app.route('/')
-def home():
-    return "Hi Everyone"
+def list1():
+    todo_list = Todo.query.all()
+    return render_template("list.html", todo_list=todo_list)
+
+@app.route('/edit')
+def edit():
+    todo_list = Todo.query.all()
+    return render_template("base.html")
 
 # base
 @app.route('/base')
-def home1():
+def base():
     return render_template("base.html")
 
 if __name__ == "__main__":
