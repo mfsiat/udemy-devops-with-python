69d98a2104e8eed4988a4d2c32fc10db8dcf32b7
mfsiat <nasirul.islam@northsouth.edu>
2021-10-24 01:04:27 +0600

    add basic test case

---
+++ test.py
@@ -0,0 +1,17 @@
+from app import app
+
+
+def test():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+
+def test2():
+    response = app.test_client().get('/edit')
+    assert response.status_code == 200
+
+# test for checking whether a text is present or not
+def test3():
+    response = app.test_client().get('/edit')
+    assert b"To" in response.data
+    assert b"Do" in response.data
+    assert b"App" in response.data
